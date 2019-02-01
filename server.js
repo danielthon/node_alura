15812150f93af8bccd5f743a0d2655b1b3ca2482
Daniel Pinheiro <danielfpinheiro98@gmail.com>
2019-01-31 23:15:33 -0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, function() {
+    console.log(`Servidor rodando na porta 3000`);
+});
+
+app.get('/', function(req, resp) {
+    resp.send(
+        `
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1>Teste Testando a Testa</h1>
+            </body>
+        </html>
+        `
+    );
+});
+
+app.get('/livros', function(req, resp) {
+    resp.send(
+        `
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1>Livrosssss</h1>
+            </body>
+        </html>
+        `
+    );
+});
+
+/*
+const http = require('http');
+
+const servidor = http.createServer(function (req, resp) {
+    
+    let html = '';
+
+    if(req.url == '/') {
+        html = `
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1>Teste Testando a Testa</h1>
+            </body>
+        </html>
+        `;
+    } else if (req.url == '/livros') {
+        html = `
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1>Livrosssss</h1>
+            </body>
+        </html>
+        `;
+    }
+    
+    resp.end(html);
+});
+servidor.listen(3000);
+*/
---
+++ test.js
@@ -0,0 +1,1 @@
+console.log("teste testando a testa");
